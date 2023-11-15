d2c5b0becdee988ae92a71453435343e0749f492
Paula Pardo <paulapardo9706@gmail.com>
2023-11-15 12:03:03 -0500

    suma de dos numero digitados por el usuario

--- Suma/src/suma.java
+++ Suma/src/suma.java
@@ -4,6 +4,21 @@ public class suma {
 
     public static void main(String[] args) {
         Scanner scaner=new Scanner(System.in);
+        int numero1;
+        int numero2;
+        String mensaje1 ="digite 2 numero enteros";
+        System.out.println(mensaje1);
+        String digiten1="digite su primer numero entero";
+        System.out.println(digiten1);
+        numero1=scaner.nextInt();
+        String digite2="digite su segundo numero entero";
+        System.out.println(digite2);
+        numero2=scaner.nextInt();
+        String mensajeResultado="el resultado es=";
+        System.out.println(mensajeResultado);
+        System.out.println(numero1+numero2);
+
+
 
     }
 }
